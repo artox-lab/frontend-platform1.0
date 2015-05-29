45540e359e8c799d632144b41ce56ef279e13b9f
Alexandr Kulitsky <to.kulitsky@outlook.com>
2015-05-29 16:31:29 +0300

    Добавлен модуль node-приложения для рендеринга клиента

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('*', routes);
 
 app.use(function(req, res, next) {
 	var err = new Error('Not Found');
